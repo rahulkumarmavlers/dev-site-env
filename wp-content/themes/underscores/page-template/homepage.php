3e86fa0ba342a57251191cc8d3e87f05297b5ff1
Rahul Kumar <rahul.kumar@mavlers.com>
2025-05-30 23:07:27 +0530

    homepage template updated

--- wp-content/themes/underscores/page-template/homepage.php
+++ wp-content/themes/underscores/page-template/homepage.php
@@ -10,6 +10,7 @@ Template Name: Homepage Template
 		<div class="col-md-8">
             
             <h1>Home page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet placeat delectus nam deleniti cupiditate est quis rem aut ex minima eos repudiandae dolores voluptas blanditiis omnis, ipsum deserunt, consequuntur nihil?</p>
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<?php the_content();?>
